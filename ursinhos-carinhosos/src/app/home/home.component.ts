4c78c90b11504816ded3717515b0c0d5af6f5445
vinicius.silva <daftwarz@gmail.com>
2022-06-21 11:24:24 -0300

    correcao

--- ursinhos-carinhosos/src/app/home/home.component.ts
+++ ursinhos-carinhosos/src/app/home/home.component.ts
@@ -15,4 +15,21 @@ export class HomeComponent implements OnInit {
     const valor = this.cpf;
     this.cpf = 'inválido';
   }
+  private extractNumbers(s: string): string {
+    return s ? s.replace(/\D)+/g, '') : '';
+  }
+
+  private mod11(digits: number[], multipliers: number[]): number {
+    let i = 0;
+    const rest = digits.reduce((p, e) => p + e * multipliers[i++], 0) % 11;
+    return rest > 9 ? 0 : rest;
+  }
+  private  extractNumbersToList(value:String):number[] {
+    let digits = Array<number>;
+    for (item : extractNumbers(value).toCharArray()) {
+        digits.push.parseInt(item);
+    }
+    return digits;
+}
+
 }
